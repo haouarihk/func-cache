ee1aef0482ceec6d5f5a74a459145b7d884e7310
Haouari haitam Kouider <haithem2001@gmail.com>
2021-05-16 13:32:12 +0100

    adding the function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,33 @@
-import { sum } from "./";
+import cache from "./";
 
-describe("blah", () => {
-  it("works", () => {
-    expect(sum(1, 1)).toEqual(2);
+const cachedFibonacci = cache(fibonacci);
+function fibonacci(num: number) {
+  let a = 1, b = 0, temp;
+
+  while (num >= 0) {
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+
+  return b;
+}
+
+describe("testing cache", () => {
+  it("fibonacci", () => {
+    const t0 = performance.now()
+
+    const val1 = cachedFibonacci(1000)
+
+    const t1 = performance.now()
+
+    const val2 = cachedFibonacci(1000)
+
+    const t2 = performance.now()
+
+    expect(t2 - t1 < t1 - t0).toBe(true);
+
+    expect(val1).toEqual(val2);
   });
 });
