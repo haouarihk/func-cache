5c9c5d3b6b67e7ff00f66ed65438d0aab3aac1ea
Haouari haitam Kouider <haithem2001@gmail.com>
2021-05-16 13:38:30 +0100

    ah

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default function cache(func: Function): typeof func | any {
   const cached: any = {};
 
-  return (...args: any): typeof func => {
+  return (...args: any) => {
     const str = args.join(',');
 
     if (cached[str]) return cached[str];
